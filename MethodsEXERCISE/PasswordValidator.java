91659e0bb798d091a727dc07caf6f7b4c05b3f7d
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-04-11 13:02:21 +0300

    methods
    new exercise

---
+++ MethodsEXERCISE/PasswordValidator.java
@@ -0,0 +1,33 @@
+package MethodsEXERCISE;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+            if (isPalindrome(command)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+
+            command = scanner.nextLine();
+
+        }
+
+    }
+
+    public static boolean isPalindrome(String text) {
+        String reversedText = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reversedText += text.charAt(i);
+
+        }
+
+        return text.equals(reversedText);
+    }
+}
