eb922655889434f4c8a563a301aaedb678a25c64
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-04-17 11:22:22 +0300

    lists
    new task

---
+++ ListsLAB/MergingLists.java
@@ -0,0 +1,48 @@
+package ListsLAB;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class MergingLists {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> firstList = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> secondList = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> result = new ArrayList<>();
+
+        if (firstList.size() >= secondList.size()){
+
+            for (int i = 0; i < firstList.size(); i++) {
+                result.add(firstList.get(i));
+
+                if (i < secondList.size()){
+                    result.add(secondList.get(i));
+                }
+
+            }
+        } else {
+            for (int i = 0; i < secondList.size(); i++) {
+
+                if (i < firstList.size()){
+                    result.add(firstList.get(i));
+                }
+
+                result.add(secondList.get(i));
+
+            }
+        }
+        System.out.print(result.toString().replace("[", "")
+                .replace("]", "")
+                .replace(",", ""));
+    }
+}
