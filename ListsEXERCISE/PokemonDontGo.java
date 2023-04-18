6bd23bdf33cc3bb2dc7e9584ca37c99b203a452c
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-04-18 17:12:02 +0300

    list
    new task

---
+++ ListsEXERCISE/PokemonDontGo.java
@@ -0,0 +1,65 @@
+package ListsEXERCISE;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PokemonDontGo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int sum = 0;
+
+        while (numbers.size() > 0) {
+            int index = Integer.parseInt(scanner.nextLine());
+
+            if (index < 0) {
+
+                int firstElement = numbers.get(0);
+                numbers.remove(0);
+                sum += firstElement;
+                int lastElement = numbers.get(numbers.size() - 1);
+                numbers.add(0, lastElement);
+                modifyList(numbers, firstElement);
+
+            } else if (index > numbers.size() - 1) {
+
+                int lastElement = numbers.get(numbers.size() - 1);
+                numbers.remove(numbers.size() - 1);
+                sum += lastElement;
+                int firstElement = numbers.get(0);
+                numbers.add(firstElement);
+                modifyList(numbers, lastElement);
+
+
+            } else if (index >= 0 && index <= numbers.size() - 1) {
+                int removedNumber = numbers.get(index);
+                sum += removedNumber;
+                numbers.remove(index);
+                modifyList(numbers, removedNumber);
+
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    private static void modifyList(List<Integer> numbers, int removedNum) {
+        for (int i = 0; i <= numbers.size() - 1 ; i++) {
+            int currentElement = numbers.get(i);
+
+            if (currentElement <= removedNum){
+                currentElement += removedNum;
+            } else {
+                currentElement -= removedNum;
+            }
+
+            numbers.set(i, currentElement);
+
+        }
+    }
+}
