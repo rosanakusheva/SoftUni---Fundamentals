ae0b388c993b669016dcd4515dd5ef3c5908e00f
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-05-07 11:59:50 +0300

    Classes and objects EXERCISE
    Solution to the 'Advertisement message' task

---
+++ ClassesAndObjectsEXERCISE/Advertisment/Main.java
@@ -0,0 +1,19 @@
+package ClassesAndObjectsEXERCISE.Advertisment;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Message message = new Message();
+
+        for (int i = 1; i <= n ; i++) {
+            String output = message.randomMessage();
+            System.out.println(output);
+
+
+        }
+    }
+}
