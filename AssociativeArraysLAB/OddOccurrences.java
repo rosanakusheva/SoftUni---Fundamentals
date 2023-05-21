348bd4a93b3ae7b64a49575fbe52d679b3e2f35a
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-05-21 17:48:50 +0300

    Associative Arrays LAB
    Solution to the 'Odd Occurrences' task

---
+++ AssociativeArraysLAB/OddOccurrences.java
@@ -0,0 +1,35 @@
+package AssociativeArraysLAB;
+
+import java.util.*;
+
+public class OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] inputArr = scanner.nextLine().split(" ");
+
+        LinkedHashMap<String, Integer> wordsMap = new LinkedHashMap<>();
+        for (String item : inputArr) {
+
+            item = item.toLowerCase();
+            wordsMap.putIfAbsent(item, 0);
+            wordsMap.put(item, wordsMap.get(item) + 1);
+
+//            if (!wordsMap.containsKey(item)) {
+//                wordsMap.put(item, 1);
+//            } else {
+//                int value = wordsMap.get(item);
+//                wordsMap.put(item, value + 1);
+//            }
+        }
+        List<String> resultList = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                resultList.add(entry.getKey());
+            }
+        }
+        System.out.println(String.join(", ", resultList));
+
+
+    }
+}
