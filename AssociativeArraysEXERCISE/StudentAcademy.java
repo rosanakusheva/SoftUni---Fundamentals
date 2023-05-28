22d41fb6dd2bdb2f97a1928b41c94bb6cd5e4a22
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-05-28 11:46:16 +0300

    Maps Exercise
    Solution to the 'Student Academy' task

---
+++ AssociativeArraysEXERCISE/StudentAcademy.java
@@ -0,0 +1,52 @@
+package AssociativeArraysEXERCISE;
+
+import java.util.*;
+
+public class StudentAcademy {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, List<Double>> studentsMap = new LinkedHashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            String studentName = scanner.nextLine();
+            double grade = Double.parseDouble(scanner.nextLine());
+
+            if (!studentsMap.containsKey(studentName)) {
+                studentsMap.put(studentName, new ArrayList<>());
+                studentsMap.get(studentName).add(grade);
+            } else {
+                studentsMap.get(studentName).add(grade);
+            }
+
+        }
+        Map<String, Double> studentAverageGrade = new LinkedHashMap<>();
+        for (Map.Entry<String, List<Double>> entry : studentsMap.entrySet()) {
+            //entry -> име (String) + списък с оценки (list<double>)
+            String studentName = entry.getKey();
+            List<Double> grades = entry.getValue();
+            double averageGrade = getAverageGrade(grades);
+            if (averageGrade >= 4.50) {
+                studentAverageGrade.put(studentName, averageGrade);
+            }
+        }
+        //разменяме записите ако на втория оценката > оценката на първия
+        studentAverageGrade.entrySet()
+                .forEach(entry -> System.out.printf("%s -> %.2f%n", entry.getKey(), entry.getValue()));
+
+    }
+
+    private static double getAverageGrade(List<Double> grades) {
+        double sumGrades = 0;
+        for (double grade : grades) {
+            sumGrades += grade;
+        }
+        return sumGrades / grades.size();
+    }
+}
+
+
+
+
+
+
