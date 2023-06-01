154d4f3e6b10c3d862879db38382d49869c4e5af
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-06-01 19:35:30 +0300

    Text Processing LAB
    Solution to the 'Repeat String' task

---
+++ TextProcessingLAB/RepeatStrings.java
@@ -0,0 +1,29 @@
+package TextProcessingLAB;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RepeatStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] wordsArr= scanner.nextLine().split(" ");
+
+        List<String> repeatList = new ArrayList<>();
+
+        for (int i = 0; i < wordsArr.length; i++) {
+            String currentWord = wordsArr[i];
+            int length = currentWord.length();
+            String repeatWord = repeatString(length, currentWord);
+            repeatList.add(repeatWord);
+        }
+        System.out.println(String.join("",repeatList));
+    }
+    public static String repeatString (int n, String word){
+        String result = "";
+        for (int i = 0; i < n; i++) {
+         result = result + word;
+        }
+        return result;
+    }
+}
