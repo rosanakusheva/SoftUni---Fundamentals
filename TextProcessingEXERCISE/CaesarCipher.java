de2b609819989dc55306f1280b4931bb29f236a7
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-06-05 20:01:50 +0300

    Text Processing EXERCISE
    Solution to the 'Caesar Cipher' task

---
+++ TextProcessingEXERCISE/CaesarCipher.java
@@ -0,0 +1,17 @@
+package TextProcessingEXERCISE;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (char symbol : text.toCharArray()){
+            char encryptedSymbol = (char)(symbol + 3);
+            encryptedText.append(encryptedSymbol);
+        }
+        System.out.println(encryptedText);
+    }
+}
