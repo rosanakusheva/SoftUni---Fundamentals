0d0465cb76d8a1f07f46c033515bce0b6cee11f0
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-06-05 19:27:45 +0300

    Text Processing EXERCISE
    Solution to the 'Valid Usernames' task

---
+++ TextProcessingEXERCISE/ValidUsernames.java
@@ -0,0 +1,32 @@
+package TextProcessingEXERCISE;
+
+import java.util.Scanner;
+
+public class ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] usernames = input.split(", ");
+
+        for (String username : usernames) {
+            if (isValidUsername(username)) {
+                System.out.println(username);
+            }
+        }
+
+    }
+
+    private static boolean isValidUsername(String username) {
+        if (username.length() < 3 || username.length() > 16) {
+            return false;
+        }
+
+        for (char symbol : username.toCharArray()) {
+            if (!Character.isLetterOrDigit(symbol) && symbol != '-' && symbol != '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
