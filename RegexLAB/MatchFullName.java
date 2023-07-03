10de49d3237052b647dae4c5e2da83f8ef348e3a
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-07-03 20:41:38 +0300

    Regex LAB
    Solution to the 'Match Full Name' task

---
+++ RegexLAB/MatchFullName.java
@@ -0,0 +1,21 @@
+package RegexLAB;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            String fullName = matcher.group();
+            System.out.print(fullName + " ");
+        }
+    }
+}
