8c42adfc56698c185e26eca9cbe3e96f90c1444d
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-07-05 19:20:18 +0300

    Regex Exercise
    Solution to the 'Furniture' task

---
+++ RegexEXERCISE/Furniture.java
@@ -0,0 +1,39 @@
+package RegexEXERCISE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Furniture {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> furniture = new ArrayList<>();
+        double totalSum = 0;
+        String regex = ">>(?<furnitureName>[A-Za-z]+)<<(?<price>[0-9]+\\.?[0-9]*)!(?<quantity>[0-9]+)";
+        Pattern pattern = Pattern.compile(regex);
+
+        String input = scanner.nextLine();
+        while (!input.equals("Purchase")) {
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()){
+                String furnitureName = matcher.group("furnitureName");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+
+                furniture.add(furnitureName);
+                double currentFurniturePrice = price * quantity;
+                totalSum += currentFurniturePrice;
+
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println("Bought furniture:");
+        furniture.forEach(System.out::println);
+        System.out.printf("Total money spend: %.2f", totalSum);
+    }
+}
