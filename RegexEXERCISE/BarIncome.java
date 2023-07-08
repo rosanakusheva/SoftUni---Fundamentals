fe0e3422aa8604fd163eef080cb88a3f9d7ae095
rosanakusheva <127954205+rosanakusheva@users.noreply.github.com>
2023-07-08 14:17:13 +0300

    Regex EXERCISE
    Solution to 'Bar Income' task

---
+++ RegexEXERCISE/BarIncome.java
@@ -0,0 +1,33 @@
+package RegexEXERCISE;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BarIncome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String regex = "%(?<customerName>[A-Z][a-z]*)%[^|$%.]*<(?<product>\\w+)>[^|$%.]*\\|(?<count>[0-9]+)\\|[^|$%.]*?(?<price>[0-9]+\\.?[0-9]*)\\$";
+        Pattern pattern = Pattern.compile(regex);
+
+        double totalIncome = 0;
+        while (!input.equals("end of shift")) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                String customer = matcher.group("customerName");
+                String product = matcher.group("product");
+                int count = Integer.parseInt(matcher.group("count"));
+                double price = Double.parseDouble(matcher.group("price"));
+
+                double totalPrice = count * price;
+                totalIncome += totalPrice;
+                System.out.printf("%s: %s - %.2f%n", customer, product, totalPrice);
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.printf("Total income: %.2f", totalIncome);
+    }
+}
